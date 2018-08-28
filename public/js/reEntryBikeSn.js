edd8a52f9e26268d4ee101a9b4fff3a4afd631d8
吴才龙 <hengxingyema@163.com>
2018-08-28 15:06:57 +0800

    增加车辆更换sn号界面1

--- public/js/reEntryBikeSn.js
+++ public/js/reEntryBikeSn.js
@@ -11,7 +11,7 @@ app.controller('reEntryBikeSnCtrl', function ($scope,$http,Md5,Base64,Sha1) {
 
         $scope.netRequestState = 'start';
         $http.post("http://yw.mimacx.com:2000/Peration/Login", {
-            withCredentials:true,
+            
             "UserName" : $scope.mimaYunweiAccount,
             "UserPass" : $scope.mimaYunweiMima,
             "mimacxtimeSpan" : $scope.timestamp,
